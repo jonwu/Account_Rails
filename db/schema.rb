9526b8ce17f26fc1275454a1e1c992039caebf00
jonwu <jonwu92@gmail.com>
2014-09-25 03:43:45 -0700

    change db settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923050833) do
+ActiveRecord::Schema.define(version: 20140925103332) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "user"
